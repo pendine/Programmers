2136efbb8b82c0c8522c89af342a93f3e1ef3563
pendine <neokin7734@gmail.com>
2019-07-07 15:16:46 +0900

    Create StockAlgorithm.java

---
+++ StockAlgorithm.java
@@ -0,0 +1,14 @@
+public class StockAlgorithm {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
